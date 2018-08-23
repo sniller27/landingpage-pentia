b0daa4e4a0452ae8d9573e87c99fd4714378c858
jonas <Jonas_Falk.Jakobsen@Student.Reutlingen-University.DE>
2018-08-23 14:04:05 +0200

    navbar click selector changed + css dots added

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,4 +1,5 @@
 // Select DOM Items
+const buttonPane = document.querySelector('.button-pane');
 const navbar = document.querySelector('nav');
 const burgerMenu = document.querySelector('.burger-menu');
 const leftMenu = document.querySelector('.left-menu');
@@ -6,31 +7,28 @@ const leftMenu = document.querySelector('.left-menu');
 // Set Initial State Of Menu
 let showMenu = false;
 
-navbar.addEventListener('click', toggleMenu);
+buttonPane.addEventListener('click', toggleMenu);
 
-function toggleMenu() {
+function toggleMenu(e) {
+
   if (!showMenu) {
+
     burgerMenu.classList.add('menu-open');
     navbar.classList.add('menu-open');
     leftMenu.classList.add('menu-open');
-    // menu.classList.add('show');
-    // menuNav.classList.add('show');
-    // menuBranding.classList.add('show');
-    // navItems.forEach(item => item.classList.add('show'));
 
     // Set Menu State
     showMenu = true;
+
   } else {
+
     burgerMenu.classList.remove('menu-open');
     navbar.classList.remove('menu-open');
     leftMenu.classList.remove('menu-open');
-    // menu.classList.remove('show');
-    // menuNav.classList.remove('show');
-    // menuBranding.classList.remove('show');
-    // navItems.forEach(item => item.classList.remove('show'));
 
     // Set Menu State
     showMenu = false;
+
   }
 	
 }
